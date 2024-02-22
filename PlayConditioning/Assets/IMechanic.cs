0b5bb63a74dc7daf38e4ddd6efe82230bcd58723
Fluffy-llama7 <Trina8767@gmail.com>
2024-02-22 00:46:18 -0800

    Interface updated

--- PlayConditioning/Assets/IMechanic.cs
+++ PlayConditioning/Assets/IMechanic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public interface IMechanic
 {
     GameObject Player { get; }
+    string Mechanic { get; }
     void Damage(float damage);
     void OnLeftClick();
     void OnRightClick();
