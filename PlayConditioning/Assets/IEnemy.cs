fe4af3248de0830f60e1bcb5ba1e2f06edc5d087
Fluffy-llama7 <Trina8767@gmail.com>
2024-03-02 20:56:02 -0800

    Interface changes

--- PlayConditioning/Assets/IEnemy.cs
+++ PlayConditioning/Assets/IEnemy.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mech
+namespace Enemy
 {
-    public interface IMechanic
+    public interface IEnemy
     {
-        void Execute(); //execute function of mechanic
+        void TakeDamage(); //action when enemy is hit
         void Update(); //function to call on every frame
-
+        void Attack(); //action when attacking
     }
 }
-
