1dd3afbaf16f336d6ad7c39f67d11689875140f3
Esther Cheng <esther588@berkeley.edu>
2024-03-03 20:44:59 -0800

    Set up TracePath class for version3 mechanic 1

---
+++ PlayConditioning/Assets/TracePath.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Mech;
+
+public class TracePath : MonoBehaviour, IMechanic
+{
+    void Awake()
+    {
+        
+    }
+
+    /// <summary>
+    /// Traces the path of the player
+    /// </summary>
+    public void Execute()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Stops tracing the path
+    /// </summary>
+    public void Stop()
+    {
+        
+    }
+}
