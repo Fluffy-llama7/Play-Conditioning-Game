9f25b76e80ea2b2911167cb8857773d2fbe5f11c
Esther Cheng <esther588@berkeley.edu>
2024-03-03 20:45:29 -0800

    Set up ChargeBlast class for version3 mechanic 2

---
+++ PlayConditioning/Assets/ChargeBlast.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Mech;
+
+public class ChargeBlast : MonoBehaviour, IMechanic
+{
+    void Awake()
+    {
+        
+    }
+
+    /// <summary>
+    /// Charges up the blast
+    /// </summary>
+    public void Execute()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Stops charging up the blast
+    /// </summary>
+    public void Stop()
+    {
+        
+    }
+}
